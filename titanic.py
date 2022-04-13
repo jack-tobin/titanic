4e7bcd2cf5859ccddac623bee06b52b55a298ecb
Jack Tobin <jtobin@192.168.1.104>
2022-04-13 11:35:19 +0100

    initial commit

---
+++ titanic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 11:33:37 2022
+
+@author: jtobin
+"""
+
+import os
+os.chdir(os.path.expanduser('~/Documents/projects/titanic'))
